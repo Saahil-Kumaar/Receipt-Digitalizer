a03afb6558de999cb9d9cf4615fe848e13af7ec1
Sahil Kumar <166407765+Saahil-Kumaar@users.noreply.github.com>
2025-05-15 22:48:21 -0700

    Update about.py

--- about.py
+++ about.py
@@ -4,6 +4,9 @@ def app():
     st.title("Receipt-Digitalizer")
     st.subheader('is a website created for users to digitize their receipts and store them in a secure database.')
     st.markdown('Created by: [Sahil Kumar](https://github.com/Saahil-Kumaar)')
-    st.markdown('Contact via mail: [sahill.kumaarr@gmail.com]')
-
+    # st.markdown('Contact via mail: [sahill.kumaarr@gmail.com]')
+    st.markdown('Created by: [Suryansh Sood](https://github.com/KnomonK)')
+    st.markdown('Created by: [Krish Gautam](https://github.com/krish-gautam117)')
+    st.markdown('Created by: [Sezal Lagwal](https://github.com/sezallagwal)')
+    st.markdown('Created by: [Yuvraj Sen](https://github.com/yuvraj2825)')
     st.markdown('Created by: [Amol](https://github.com/05-amol)')
